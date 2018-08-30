338890145d50dd5ddf5970ef03dc0ca323e31741
Malahini Solutions <malahinisolutions@gmail.com>
2018-08-30 19:05:27 +0530

    23

--- src/Commands/SystemCommands/TestCommand.php
+++ src/Commands/SystemCommands/TestCommand.php
@@ -17,9 +17,10 @@ class TestCommand extends SystemCommand
         $message = $this->getMessage();            // Get Message object
 
         $chat_id = $message->getChat()->getId();   // Get the current Chat ID
-		$command_str = trim($message->getText(true));
+
+        $command_str = trim($message->getText(true));
         $data = [                                  // Set up the new message data
-            'chat_id' => $chat_id,                 // Set Chat ID to send the message to
+            'chat_id' => '-236744087',                 // Set Chat ID to send the message to
             'text'    => $command_str,//'This is just a Test group message...', // Set message to send
         ];
 
