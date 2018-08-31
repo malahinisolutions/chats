ccf3f094e3d7d5335df6c22d5422cd5bf419312e
Malahini Solutions <malahinisolutions@gmail.com>
2018-08-31 16:07:54 +0530

    31

--- hook.php
+++ hook.php
@@ -17,7 +17,7 @@ $bot_username = 'coinsample_bot';
 
 // Define all IDs of admin users in this array (leave as empty array if not used)
 $admin_users = [
-    690920531,
+    690920531,621627834,
 ];
 
 // Define all paths for your custom commands in this array (leave as empty array if not used)
